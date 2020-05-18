f2cdb81e97f9d725e73dab9b31c5563648060c7d
linghu <linghuwangyi@163.com>
2020-05-18 21:49:59 +0800

    将git_test1添加到本地仓库

---
+++ git_test1/src/main/java/cn/ljp/git/test/GitTest.java
@@ -0,0 +1,14 @@
+package cn.ljp.git.test;
+
+/**
+ * create by ljb  on 2020/5/1821:39
+ * git_test1--GitTest
+ * function
+ * design
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
